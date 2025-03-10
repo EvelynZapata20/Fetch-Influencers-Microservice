dfc9d21107c188b7984ba32742f31a9188326911
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-09 21:54:58 -0500

    refactor: change hashtag scraper to search videos in the search functionality of TikTok

--- app/services/scrapers/hashtag_scraper.py
+++ app/services/scrapers/hashtag_scraper.py
@@ -1,55 +1,38 @@
-"""
-TikTok Hashtag Scraper
-
-This script automates the extraction of unique usernames from videos 
-under a TikTok hashtag page using Selenium
-"""
-from app.utils.webdriver import init_webdriver
+from app.utils.web_driver import WebDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import logging
-
-logger = logging.getLogger(__name__)
-
-def get_usernames(tag_url: str) -> set:
-    """
-    Scrapes TikTok to extract unique usernames from a hashtag page.
-    
-    Args:
-        url (str): The TikTok tag URL to scrape.
-    
-    Returns:
-        set: A set of unique usernames found on the page.
-    """
-
-    driver = init_webdriver()
-    wait = WebDriverWait(driver, 10)
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import time
+import re
+import random
+def get_usernames(url_hashtag):
+    driver = WebDriverManager.get_driver()
     usernames = set()
 
     try:
-        driver.get(tag_url)
+        driver.get(url_hashtag)
+        wait = WebDriverWait(driver, random.uniform(8, 12))
         
-        videos = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[data-e2e="challenge-item"]')))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-e2e="search_video-item"]')))
+        
+        for _ in range(10):  
+            driver.execute_script("window.scrollBy(0, 2000);")
+            time.sleep(random.uniform(1.5, 3.5))
 
+        videos = driver.find_elements(By.CSS_SELECTOR, '[data-e2e="search_video-item"]')
         for video in videos:
             try:
-                username_element = video.find_element(By.CSS_SELECTOR, '[data-e2e="challenge-item-username"]')
-                username = username_element.text
-                if username:
-                    usernames.add(username)
+                link_element = video.find_element(By.TAG_NAME, "a")
+                video_url = link_element.get_attribute("href")
+                
+                match = re.search(r"@([^/]+)/video", video_url)
+                username = match.group(1) if match else ""
+                usernames.add(username)
             except NoSuchElementException:
-                continue 
+                continue
 
     except (TimeoutException, NoSuchElementException) as e:
-        logger.error(f"Error retrieving hashtag data: {e}")
-        return None
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        return None
+        print(f"Error in hashtag module: {e}")
 
-    finally:    
-        driver.quit()
-    
     return usernames
