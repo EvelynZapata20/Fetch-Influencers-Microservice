d2ec4b64e44deff647fdbfb6a5a1ab6c92deeaff
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-09 21:51:53 -0500

    refactor: modify web driver to avoid captchas and manage country context

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.routes.routes import router
+from app.utils.web_driver import WebDriverManager
 
 app = FastAPI(title="TikTok Scraper API", version="1.0")
 
 app.include_router(router)
+
+driver = WebDriverManager.get_driver()
+
 
 @app.get("/")
 def root():
