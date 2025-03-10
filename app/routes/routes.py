a916bc28be6dfad519000867b90defb5ecc1328a
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-09 21:56:39 -0500

    refactor: change name of route

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from app.use_cases.scraper import scrape_city_data
+from app.use_cases.fetch_tiktok_data import fetch_influencers_by_tag
 from app.schemas.influencer import InfluencersList
 
 router = APIRouter()
 
-@router.get("/scrape/{city}", response_model=InfluencersList)
-def scrape(city: str):
-    return scrape_city_data(city)
+@router.get("/api/v1/influencers/{city}", response_model=InfluencersList)
+def get_influencers(city: str):
+    return fetch_influencers_by_tag(city)
